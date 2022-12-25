cf2599eafaa8eaa96c2883f87600decd32260e34
Ruthvikbr-MM <ruthvik.br@mutualmobile.com>
2022-12-25 12:37:18 +0530

    Add ViewModel Tests

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/addedittask/TaskViewModelTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/addedittask/TaskViewModelTest.kt
@@ -6,9 +6,9 @@ import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.android.architecture.blueprints.todoapp.Event
 import com.example.android.architecture.blueprints.todoapp.getOrAwaitValue
+import com.example.android.architecture.blueprints.todoapp.tasks.TasksFilterType
 import com.example.android.architecture.blueprints.todoapp.tasks.TasksViewModel
-import org.hamcrest.Matchers.not
-import org.hamcrest.Matchers.nullValue
+import org.hamcrest.Matchers.* // ktlint-disable no-wildcard-imports
 import org.junit.Assert.assertThat
 import org.junit.Before
 import org.junit.Rule
@@ -18,7 +18,7 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class TaskViewModelTest {
 
-    lateinit var taskViewModel: TasksViewModel
+    private lateinit var taskViewModel: TasksViewModel
 
     @get:Rule
     var instantExecutorRule = InstantTaskExecutorRule()
@@ -40,5 +40,11 @@ class TaskViewModelTest {
         } finally {
             taskViewModel.newTaskEvent.removeObserver(observer)
         }
+    }
+
+    @Test
+    fun `Setting Filter to show all tasks displays the Add Task Button`() {
+        taskViewModel.setFiltering(TasksFilterType.ALL_TASKS)
+        assertThat(taskViewModel.tasksAddViewVisible.getOrAwaitValue(), `is`(true))
     }
 }
